627b597de87319b341a31180f7e54de62ef5db70
Daniel Johansson <danne.joh1@gmail.com>
2018-06-22 16:18:59 -0700

    added a test case scenario generator

---
+++ sim/GenerateRandomSessions.py
@@ -0,0 +1,40 @@
+'''
+Script for generating a test case scenario.
+This scenario is stored in a pickled file that will be read when
+the simulator starts.
+'''
+
+import pickle
+from datetime import datetime, timedelta
+import random
+
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = random.randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
+file_name = 'test_session.p'
+sessions = []
+
+start_time = datetime.now()
+end_time = start_time + timedelta(days=1)
+dt = end_time - start_time
+delta_days = dt.days
+
+print(start_time.timestamp())
+
+for i in range(1,10):
+    arrival_time = random_date(start_time, end_time)
+    departure_time = random_date(arrival_time, arrival_time + timedelta(days=1))
+    requested_energy = random.uniform(1,10)
+    station_id = i
+    sessions.append([arrival_time, departure_time, requested_energy, station_id])
+
+pickle.dump(sessions, open( file_name, "wb"))
+
+
