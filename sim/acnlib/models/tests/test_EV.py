68feed64bc15c6a02962e90d07b852fb2f08a12b
zach401 <zach401@gmail.com>
2018-10-09 15:20:10 -0700

    Added tests for all models.

---
+++ sim/acnlib/models/tests/test_EV.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+from unittest.mock import Mock, create_autospec
+from ..ev import EV
+from ..battery import Battery
+
+
+class TestEV(TestCase):
+    def setUp(self):
+        basicBatt = create_autospec(Battery)
+        self.ev = EV(0, 10, 25.0, 'PS-001', '0001', basicBatt)
+
+    def test_charge_valid_rate(self):
+        self.ev._battery.charge = Mock(return_value=16)
+        rate = self.ev.charge(16)
+        self.assertEqual(rate, 16)
+        self.assertEqual(self.ev.energy_delivered, 16)
+        self.ev._battery.charge.assert_called_once()
+
+    def test_charge_over_max_rate(self):
+        self.ev._battery.charge = Mock(return_value=32)
+        rate = self.ev.charge(40)
+        self.assertEqual(rate, 32)
+        self.assertEqual(self.ev.energy_delivered, 32)
+        self.ev._battery.charge.assert_called_once()
+
+    def test_reset(self):
+        self.ev._battery.reset = Mock()
+        self.ev.reset()
+        self.assertEqual(self.ev.energy_delivered, 0)
+        self.ev._battery.reset.assert_called_once()
+
---
+++ sim/acnlib/models/tests/test_EVSE.py
@@ -0,0 +1,68 @@
+from unittest import TestCase
+from unittest.mock import Mock, create_autospec
+from ..ev import EV
+from ..evse import EVSE, FiniteRatesEVSE, InvalidRateError, Empty, StationOccupiuedError
+
+
+class TestEVSE(TestCase):
+    def setUp(self):
+        self.evse = EVSE('0001')
+
+    def test_plugin_unoccupied(self):
+        ev = create_autospec(EV)
+        self.evse.plugin(ev)
+        self.assertEqual(self.evse.EV, ev)
+
+    def test_plugin_occupied(self):
+        ev = create_autospec(EV)
+        ev2 = create_autospec(EV)
+        self.evse.plugin(ev)
+        self.assertEqual(self.evse.EV, ev)
+        with self.assertRaises(StationOccupiuedError):
+            self.evse.plugin(ev2)
+
+    def test_unplug_occupied(self):
+        ev = create_autospec(EV)
+        self.evse.plugin(ev)
+        self.evse.unplug()
+        self.assertIsNone(self.evse.EV)
+
+    def test_unplug_unoccupied(self):
+        self.evse.unplug()
+        self.assertIsNone(self.evse.EV)
+
+    def test_set_pilot_has_ev_valid_rate(self):
+        ev = create_autospec(EV)
+        self.evse.plugin(ev)
+        self.evse.set_pilot(16)
+        self.assertEqual(self.evse.current_pilot, 16)
+        self.evse.EV.charge.assert_called_once()
+
+    def test_set_pilot_has_ev_negative_rate(self):
+        ev = create_autospec(EV)
+        self.evse.plugin(ev)
+        with self.assertRaises(InvalidRateError):
+            self.evse.set_pilot(-1)
+
+    def test_set_pilot_no_ev_valid_rate(self):
+        with self.assertRaises(Empty):
+            self.evse.set_pilot(16)
+
+    def test_set_pilot_no_ev_negative_rate(self):
+        with self.assertRaises(InvalidRateError):
+            self.evse.set_pilot(-1)
+
+
+class TestFiniteRatesEVSE(TestEVSE):
+    def setUp(self):
+        self.evse = FiniteRatesEVSE('0001', [0, 8, 16, 24, 32])
+
+    def test_set_pilot_has_ev_invalid_rate(self):
+        ev = create_autospec(EV)
+        self.evse.plugin(ev)
+        with self.assertRaises(InvalidRateError):
+            self.evse.set_pilot(17.2)
+
+    def test_set_pilot_no_ev_invalid_rate(self):
+        with self.assertRaises(InvalidRateError):
+            self.evse.set_pilot(17.2)
