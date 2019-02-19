a2320bb5cd7990d080e5b076fdf3572e8a6470aa
zach401 <zach401@gmail.com>
2019-02-19 15:40:29 -0800

    Added tests for all models.

---
+++ sim/acnlib/signals/sig_utils.py
@@ -0,0 +1,11 @@
+import math
+
+def periods_since_midnight(t, period):
+    midnight = t.replace(hour=0, minute=0, second=0, microsecond=0)
+    return int((t - midnight).total_seconds() / period)
+
+
+def extended_schedule(schedule, start, length):
+    tile_multiple = math.ceil((start + length)/len(schedule))
+    tiled_schedule = schedule*tile_multiple
+    return tiled_schedule[start:start+length]
