6dc7fa74c97b386713c9b9dffded197b3d9c5ab6
zach401 <zach401@gmail.com>
2019-02-19 15:32:38 -0800

    Added algorithms to contrib.

---
+++ contrib/acn_opt_algorithm.py
@@ -0,0 +1,194 @@
+from sim.BaseAlgorithm import BaseAlgorithm
+from contrib.gurobi_utils import OptimizationScheduler, OptimizationPostProcessor, InfeasibilityException, PHASE_AWARE
+import math
+import numpy as np
+from collections import defaultdict
+from copy import deepcopy
+
+
+FORCE_UNIQUE_COEFF = 1e-14
+
+
+def schedule_dict_to_list(sch, T):
+    sch_list = [sch[t] if t in sch else 0 for t in range(T)]
+    return sch_list
+
+
+class OptScheduler(BaseAlgorithm):
+    def __init__(self, const_scale=1, const_type=PHASE_AWARE, init_peak=0, force_unique=False, energy_equality=False):
+        super().__init__()
+        self.const_scale = const_scale
+        self.const_type = const_type
+        self.init_peak = init_peak
+        self.force_unique = force_unique
+        self.energy_equality = energy_equality
+
+    def obj(self, scheduler, length):
+        raise NotImplementedError('obj() is not defined in the base class. Please use a derived class.')
+
+    def _solve(self, active_evs):
+        if len(active_evs) == 0:
+            return {}
+
+        t = self.interface.get_current_time()
+        for ev in active_evs:
+            ev.arrival -= t
+            ev.departure -= t
+        T = int(math.ceil(np.max([ev.departure for ev in active_evs]))) + 1
+
+        sch = OptimizationScheduler(const_type=PHASE_AWARE, const_scale=self.const_scale, energy_equality=self.energy_equality)
+        sch.add_evs(active_evs)
+        sch.obj = self.obj(sch, T)
+
+        sch.compile()
+        obj, schedule, runtime = sch.solve()
+        return schedule
+
+    def schedule(self, active_evs):
+        if len(active_evs) == 0:
+            return {}
+
+        try:
+            schedule = self._solve(active_evs)
+        except InfeasibilityException:
+            print('An error occured at time {0}'.format(self.interface.get_current_time()))
+            return {}
+
+        T = int(math.ceil(np.max([ev.departure for ev in active_evs]))) + 1
+        ev_to_evse = {ev.session_id: ev.station_id for ev in active_evs}
+        full_schedule = {ev_to_evse[key]: np.clip(schedule_dict_to_list(val, T), a_min=0, a_max=None) for key, val in schedule.items()}
+        return full_schedule
+
+
+class ProfitMaximization(OptScheduler):
+    def __init__(self, dc_proportional=False, const_scale=1, const_type=PHASE_AWARE, init_peak=0, force_unique=False, energy_equality=False):
+        super().__init__(const_scale, const_type, init_peak, force_unique, energy_equality)
+        self.dc_proportional = dc_proportional
+
+    def get_peak(self):
+        return max(self.interface.get_prev_peak(), self.init_peak)
+
+    def obj(self, scheduler, length):
+        t = self.interface.get_current_time()
+        revenue = self.interface.get_revenue()
+        prices = self.interface.get_prices(t, length)
+        if self.dc_proportional:
+            # demand_charge = self.interface.get_demand_charge(length)
+            demand_charge = self.interface.simulator.prices.demand_charge / 30
+        else:
+            demand_charge = self.interface.simulator.prices.demand_charge
+        prev_peak = self.get_peak()
+        temp_obj = scheduler.max_profit_obj(revenue, prices, demand_charge, prev_peak)
+        if self.force_unique:
+            temp_obj += FORCE_UNIQUE_COEFF*scheduler.force_concave()
+        return temp_obj
+
+
+class ProfitMaximizationWithQuickCharge(ProfitMaximization):
+    def __init__(self, alpha, dc_proportional=False, const_scale=1, const_type=PHASE_AWARE, init_peak=0, force_unique=False, energy_equality=False):
+        super().__init__(dc_proportional, const_scale, const_type, init_peak, force_unique, energy_equality)
+        self.alpha = alpha
+
+    def obj(self, scheduler, length):
+        temp_obj = super().obj(scheduler, length)
+        temp_obj += (self.alpha/length)*scheduler.quick_charge_obj()
+        return temp_obj
+
+
+class ProfitMaximizationSolar(ProfitMaximizationWithQuickCharge):
+    def __init__(self, alpha, solar, dc_proportional=False, const_scale=1, const_type=PHASE_AWARE, init_peak=0, force_unique=False, energy_equality=False):
+        super().__init__(alpha, dc_proportional, const_scale, const_type, init_peak, force_unique, energy_equality)
+        self.solar_signal = solar
+
+    def obj(self, scheduler, length):
+        t = self.interface.get_current_time()
+        revenue = self.interface.get_revenue()
+        prices = self.interface.get_prices(t, length)
+        demand_charge = self.interface.get_demand_charge(length)
+        prev_peak = self.get_peak()
+        solar_gen = self.solar_signal.get_generation(t, length)
+        temp_obj = scheduler.max_profit_w_solar_obj(revenue, prices, demand_charge, solar_gen, prev_peak)
+        if self.force_unique:
+            temp_obj += FORCE_UNIQUE_COEFF*scheduler.force_concave()
+        if self.alpha > 0:
+            temp_obj += (self.alpha/length)*scheduler.quick_charge_obj()
+        return temp_obj
+
+
+class GreedySchedule(OptScheduler):
+    def obj(self, scheduler, length):
+        temp_obj = scheduler.quick_charge_obj()
+        if self.force_unique:
+            temp_obj += FORCE_UNIQUE_COEFF*scheduler.force_concave()
+        return temp_obj
+
+
+class TrackSignal(OptScheduler):
+    def __init__(self, solar, alpha=0, const_scale=1, const_type=PHASE_AWARE, init_peak=0, force_unique=False, energy_equality=False):
+        super().__init__(const_scale, const_type, init_peak, force_unique, energy_equality)
+        self.solar_signal = solar
+        self.alpha = alpha
+
+    def obj(self, scheduler, length):
+        t = self.interface.get_current_time()
+        signal = self.solar_signal.get_generation(t, length)
+        temp_obj = scheduler.tracking_obj(signal, self.alpha)
+        # if self.alpha > 0:
+        #     temp_obj += self.alpha*scheduler.quick_charge_obj()
+        # if self.force_unique:
+        #     temp_obj += FORCE_UNIQUE_COEFF * scheduler.force_concave()
+        return temp_obj
+
+
+def project_rate(rate, min_rate=6, eps=0.1):
+    proj_rate = int(rate + eps)
+    if proj_rate < min_rate:
+        proj_rate = 0
+    return proj_rate
+
+
+class PostProcessor:
+    def __init__(self, min_rate=6, cutoff=6, const_scale=1, const_type=PHASE_AWARE, init_peak=0):
+        super().__init__()
+        self.const_scale = const_scale
+        self.const_type = const_type
+        self.init_peak = init_peak
+        self.min_rate = min_rate
+        self.cutoff = cutoff
+
+    def post_process(self, active_evs, schedule):
+        if len(active_evs) == 0:
+            return {}
+
+        evs = {ev.session_id: deepcopy(ev) for ev in active_evs}
+
+        schedule_by_time = defaultdict(dict)
+        for id in schedule:
+            for t in schedule[id]:
+                schedule_by_time[t][id] = schedule[id][t]
+
+        T_min = min(schedule_by_time.keys())
+        T_max = max(schedule_by_time.keys())
+
+        processed_schedule = defaultdict(dict)
+        e_del = {ev.session_id: 0 for ev in active_evs}
+        for t in range(T_min, T_max+1):
+            if t not in schedule_by_time or len(schedule_by_time[t]) == 0:
+                continue
+            target = {id: val for id, val in schedule_by_time[t].items() if evs[id].remaining_demand - e_del[id] >= self.min_rate}
+            if len(target) == 0:
+                continue
+            sch = OptimizationPostProcessor(self.min_rate, self.cutoff, e_del, const_type=PHASE_AWARE, const_scale=self.const_scale)
+            sch.add_evs([evs[id] for id in target])
+            sch.obj = sch.post_processing_obj(target)
+
+            sch.compile()
+            obj, schedule, runtime = sch.solve()
+
+            for id in schedule:
+                rate = project_rate(schedule[id][0])
+                processed_schedule[id][t] = rate
+                e_del[id] += rate
+            # except InfeasibilityException:
+            #     print('An error occured at time {0}'.format(t))
+        return processed_schedule
---
+++ contrib/monitor.py
@@ -0,0 +1,184 @@
+import numpy as np
+import math
+import copy
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
+
+def create_pod_limits(test_case):
+    N = len(test_case)
+    CC_pod = {'CA-322', 'CA-493', 'CA-496', 'CA-320', 'CA-495', 'CA-321', 'CA-323', 'CA-494'}
+    AV_pod = {'CA-324', 'CA-325', 'CA-326', 'CA-327', 'CA-489', 'CA-490', 'CA-491', 'CA-492'}
+
+    CC_pod_group = [0 for i in range(N)]
+    AV_pod_group = [0 for i in range(N)]
+    for i in range(N):
+        if test_case[i].station_id in CC_pod:
+            CC_pod_group[i] = 1
+        if test_case[i].station_id in AV_pod:
+            AV_pod_group[i] = 1
+    return [(np.array(CC_pod_group), 80000), (np.array(AV_pod_group), 80000)]
+    # return np.array([CC_pod_group, AV_pod_group]), np.array([80000, 80000])
+
+
+def get_groupings():
+    CC_pod = {'CA-322', 'CA-493', 'CA-496', 'CA-320', 'CA-495', 'CA-321', 'CA-323', 'CA-494'}
+    AV_pod = {'CA-324', 'CA-325', 'CA-326', 'CA-327', 'CA-489', 'CA-490', 'CA-491', 'CA-492'}
+    AB = {'CA-{0}'.format(i) for i in [308, 508, 303, 513, 310, 506, 316, 500, 318, 498]}
+    AB |= CC_pod | AV_pod
+
+    BC = {'CA-{0}'.format(i) for i in [304, 512, 305, 511, 313, 503, 311, 505, 317, 499]}
+    CA = {'CA-{0}'.format(i) for i in [307, 509, 309, 507, 306, 510, 315, 501, 319, 497, 312, 504, 314, 502]}
+
+    # Temporarily add ADA spaces to BC, since we don't know where they come from
+    BC |= {'CA-148', 'CA-149', 'CA-212', 'CA-213'}
+
+    groups = {}
+    groups['ALL'] = AB | BC | CA
+    groups['AB'] = AB
+    groups['BC'] = BC
+    groups['CA'] = CA
+    groups['AV-Pod'] = AV_pod
+    groups['CC-Pod'] = CC_pod
+    return groups
+
+
+# def get_session_phase_groups(test_case):
+#     groups = get_groupings()
+#     session_groups = {}
+#     session_groups['AB'] = [ev.session_id for ev in test_case if ev.station_id in groups['AB']]
+#     session_groups['BC'] = [ev.session_id for ev in test_case if ev.station_id in groups['BC']]
+#     session_groups['CA'] = [ev.session_id for ev in test_case if ev.station_id in groups['CA']]
+#     session_groups['AV-Pod'] = [ev.session_id for ev in test_case if ev.station_id in groups['AV-Pod']]
+#     session_groups['CC-Pod'] = [ev.session_id for ev in test_case if ev.station_id in groups['CC-Pod']]
+#     return session_groups
+
+def get_session_phase_groups(ids):
+    groups = get_groupings()
+    session_groups = {}
+    session_groups['AB'] = [id for id in ids if id in groups['AB']]
+    session_groups['BC'] = [id for id in ids if id in groups['BC']]
+    session_groups['CA'] = [id for id in ids if id in groups['CA']]
+    session_groups['AV-Pod'] = [id for id in ids if id in groups['AV-Pod']]
+    session_groups['CC-Pod'] = [id for id in ids if id in groups['CC-Pod']]
+    return session_groups
+
+
+def get_phase_grouping(test_case):
+    N = len(test_case)
+    groups = get_groupings()
+
+    AB_group = [0 for i in range(N)]
+    BC_group = [0 for i in range(N)]
+    CA_group = [0 for i in range(N)]
+
+    for i in range(N):
+        if test_case[i].station_id in groups['AB']:
+            AB_group[i] = 1
+        if test_case[i].station_id in groups['BC']:
+            BC_group[i] = 1
+        if test_case[i].station_id in groups['CA']:
+            CA_group[i] = 1
+    return np.array(AB_group), np.array(BC_group), np.array(CA_group)
+
+
+def post_process(EVs, pilot, processor, processor_kwargs):
+    # Need to make a deep copy of EVs since we will be changing the EVs state
+    working_EVs = copy.deepcopy(EVs)
+
+    T = int(math.ceil(np.max([ev.departure for ev in working_EVs])))
+    post_pilot = {ev.session_id : np.zeros((T,)) for ev in working_EVs}
+    delivered = {ev.session_id : 0 for ev in working_EVs}
+    for t in range(T):
+        active_set = [ev for ev in working_EVs if t >= ev.arrival and t<= ev.departure and ev.remaining_demand() >= 6]
+        active_pilots = {ev.session_id:pilot[ev.session_id][t] for ev in active_set}
+
+        post_pilot_t = processor(active_set, active_pilots, **processor_kwargs)
+
+        for ev in active_set:
+          post_pilot[ev.session_id][t] = post_pilot_t[ev.session_id]
+          ev.delivered += post_pilot_t[ev.session_id]
+          # ev.max_rate = min(ev.max_rate, max(ev.remaining_demand(),0))
+
+    return post_pilot
+
+
+def discretize_rates(rate, inc, min_rate=0, max_rate=32):
+    """ Returns a rate rounded down to the next valid discrete rate
+
+    :param rate:        desired rate
+    :param inc:         increment between discrete rates
+    :param min_rate:    lowest rate available
+    :param max_rate:    highest rate available
+    :return:            rate rounded down to the next valid discrete rate
+    """
+    if inc > 0:
+        new_rate = int(rate/inc)*inc
+    else:
+        new_rate = rate
+    return max(min(new_rate, max_rate), min_rate)
+
+
+def get_obj(pilots):
+    T = len(pilots[0])
+    cost_vec = np.array([t - T for t in range(T)])
+    obj = 0
+    for key in pilots:
+        obj += pilots[key].dot(cost_vec)
+    return obj
+
+
+def sum_cols(df, cols):
+    if len(cols) <= 0:
+        return None
+    acc = df[cols[0]].copy()
+    for i in range(1, len(cols)):
+        acc += df[cols[i]]
+    return acc
+
+
+def get_phase_currents(rates):
+    groups = get_session_phase_groups(list(rates.keys()))
+    phase_currents = pd.DataFrame()
+    for phase in ['AB', 'BC', 'CA']:
+        phase_currents[phase] = sum_cols(rates, groups[phase])
+    return phase_currents
+
+
+def get_line_currents(rates):
+    phase_currents = get_phase_currents(rates)
+
+    # Define phase conversions
+    re_im = {'re': 1, 'im': 1j}
+    phaseAB = {'re': np.cos(np.deg2rad(30)), 'im': np.sin(np.deg2rad(30))}
+    phaseBC = {'re': np.cos(np.deg2rad(-90)), 'im': np.sin(np.deg2rad(-90))}
+    phaseCA = {'re': np.cos(np.deg2rad(150)), 'im': np.sin(np.deg2rad(150))}
+
+    line_currents = pd.DataFrame()
+    line_currents['A'] = sum((re_im[part] * phase_currents['AB'] * phaseAB[part] - re_im[part] * phase_currents['CA'] * phaseCA[part] for part in ['re', 'im']))
+    line_currents['B'] = sum((re_im[part] * phase_currents['BC'] * phaseBC[part] - re_im[part] * phase_currents['AB'] * phaseAB[part] for part in ['re', 'im']))
+    line_currents['C'] = sum((re_im[part] * phase_currents['CA'] * phaseCA[part] - re_im[part] * phase_currents['BC'] * phaseBC[part] for part in ['re', 'im']))
+    return line_currents
+
+
+def get_primary_currents(rates):
+    secondary_line_currents = get_line_currents(rates)
+    primary_line_currents = pd.DataFrame()
+    primary_line_currents['A'] = (1/4) * (secondary_line_currents['A'] - secondary_line_currents['C'])
+    primary_line_currents['B'] = (1/4) * (secondary_line_currents['B'] - secondary_line_currents['A'])
+    primary_line_currents['C'] = (1/4) * (secondary_line_currents['C'] - secondary_line_currents['B'])
+    return primary_line_currents
+
+
+def get_pod_currents(rates):
+    groups = get_session_phase_groups(list(rates.keys()))
+    pod_currents = pd.DataFrame()
+    for pod in ['AV-Pod', 'CC-Pod']:
+        pod_currents[pod] = sum_cols(rates, groups[pod])
+    return pod_currents
+
+
+
+
+
