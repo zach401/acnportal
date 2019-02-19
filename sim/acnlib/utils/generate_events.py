43456eb6832acd339d47ff56276f85d54328b920
zach401 <zach401@gmail.com>
2019-02-19 15:30:46 -0800

    Added force feasible to generate_events.

--- sim/acnlib/utils/generate_events.py
+++ sim/acnlib/utils/generate_events.py
@@ -1,14 +1,20 @@
 from acnlib.event_queue import PluginEvent
 from acnlib.models.ev import EV
 from acnlib.models.battery import Battery
+import math
 
 
-def datetime_to_timestamp(dt, period):
-    return int(dt.timestamp() / (60 * period))
+def datetime_to_timestamp(dt, period, round_up=False):
+    ts = dt.timestamp() / (60 * period)
+    if round_up:
+        return int(math.ceil(ts))
+    else:
+        return int(ts)
 
 
-def get_sessions_mongo(collection, start, end, voltage=220, max_rate=32, period=1, max_len=float('inf')):
-    docs = collection.find({'connectionTime': {'$gte': start, '$lt': end}})
+def get_sessions_mongo(collection, start, end, voltage=220, max_rate=32, period=1, max_len=float('inf'),
+                       force_feasible=False, battery_type=Battery, batt_args=None, battery_cap_fn=None):
+    docs = collection.find({'connectionTime': {'$gte': start, '$lt': end}}, {'chargingCurrent': 0, 'pilotSignal': 0}).sort('connectionTime', 1)
     start_offset = datetime_to_timestamp(start, period)
     sessions = []
     for d in docs:
@@ -18,15 +24,25 @@ def get_sessions_mongo(collection, start, end, voltage=220, max_rate=32, period=
             departure = arrival + max_len
         requested_energy = d['kWhDelivered'] * 1000 * (60/period) / voltage  # A*periods
         max_rate = max_rate
+        if force_feasible:
+            requested_energy = min(requested_energy, max_rate*(departure - arrival))
         session_id = d['sessionID']
         station_id = d['spaceID']
-        batt = Battery(requested_energy, 0, max_rate)
+        batt_args = {} if batt_args is None else batt_args
+        if battery_cap_fn is None:
+            cap = requested_energy
+            init = 0
+        else:
+            cap, init = battery_cap_fn(requested_energy, departure - arrival, voltage, period)
+        batt = battery_type(cap, init, max_rate, **batt_args)
         ev = EV(arrival, departure, requested_energy, station_id, session_id, batt)
         sessions.append(ev)
     return sessions
 
 
-def generate_test_case_mongo(collection, start, end, voltage=220, max_rate=32, period=1):
-    sessions = get_sessions_mongo(collection, start, end, voltage, max_rate, period)
+def generate_test_case_mongo(collection, start, end, voltage=220, max_rate=32, period=1, max_len=float('inf'),
+                             force_feasible=False, battery_type=Battery, batt_args=None, battery_cap_fn=None):
+    sessions = get_sessions_mongo(collection, start, end, voltage, max_rate, period, max_len, force_feasible=force_feasible,
+                                  battery_type=battery_type, batt_args=batt_args, battery_cap_fn=battery_cap_fn)
     events = [PluginEvent(sess.arrival, sess) for sess in sessions]
     return events
