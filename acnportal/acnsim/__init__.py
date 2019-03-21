4b725e8ccee0a32bb3cbc05fac8de512d56a3ad8
zach401 <zach401@gmail.com>
2019-03-21 16:07:01 -0700

    Refactoring __init__ files to make for shorter imports.

--- acnportal/acnsim/__init__.py
+++ acnportal/acnsim/__init__.py
@@ -2,5 +2,7 @@ from .simulator import Simulator
 from .simulator import InvalidScheduleError
 from .interface import Interface
 
+from .network import ChargingNetwork
+
 del simulator
 del interface
--- acnportal/acnsim/network/__init__.py
+++ acnportal/acnsim/network/__init__.py
@@ -1,7 +1,6 @@
 from .charging_network import ChargingNetwork
 from .charging_network import StationOccupiedError
 from .constraint_set import ConstraintSet
-from .constraint_set import Constraint
 from .constraint_set import Current
 
 del charging_network
