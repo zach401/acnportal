f6cde05fdb9e3d2133c47cec6e3d105d6de9776d
zach401 <zach401@gmail.com>
2019-03-21 19:53:01 -0700

    Refactored documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'ACN-Sim'
+project = 'ACN Research Portal'
 copyright = '2019, Caltech'
 author = 'Zachary Lee, Daniel Johansson'
 
