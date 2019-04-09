bff3c75b103c06e7b7d35381d73ba9762d6c4828
zach401 <zach401@gmail.com>
2019-04-08 17:26:38 -0700

    Changes to how properties were documented in battery, ev, and evse.
    Modified NoisyBattery code so that it could inherit its functionality
    from Linear2StageBattery to reduce code duplication.

--- acnportal/acnsim/models/battery.py
+++ acnportal/acnsim/models/battery.py
@@ -8,7 +8,7 @@ TWO_STAGE = 'TwoStage'
 class Battery:
     """This class models the behavior of a battery and battery management system (BMS).
 
-    Attributes:
+    Args:
         capacity (float): Capacity of the battery [acnsim units]
         init_charge (float): Initial charge of the battery [acnsim units]
         max_rate (float): Maximum charging rate of the battery [acnsim units]
@@ -24,14 +24,17 @@ class Battery:
 
     @property
     def _soc(self):
+        """ Returns the state of charge of the battery as a percent."""
         return self._current_charge / self._capacity
 
     @property
     def max_rate(self):
+        """ Returns the maximum charging rate of the battery. """
         return self._max_rate
 
     @property
     def current_charging_rate(self):
+        """ Returns the current charging rate of the battery. """
         return self._current_charging_rate
 
     def charge(self, pilot):
@@ -64,39 +67,7 @@ class Battery:
         self._current_charging_rate = 0
 
 
-class NoisyBattery(Battery):
-    """ Extends Battery to model noise in the charging process.
-
-    Noise here is modeled as rectified Gaussian noise which is subtracted from the normal maximum charging
-    rate. This noise roughly models the behavior of the battery management system which might restrict battery
-    charging rate during the charging process.
-
-    All public attributes are the same as Battery.
-    """
-
-    def __init__(self, capacity, init_charge, max_rate, noise_level=0):
-        super().__init__(capacity, init_charge, max_rate)
-        self._noise_level = noise_level
-
-    def charge(self, pilot):
-        """ Method to "charge" the battery include subtractive rectified Gaussian noise.
-
-        Args:
-            pilot (float): Pilot signal passed to the battery.
-
-        Returns:
-            float: actual charging rate of the battery.
-
-        """
-        charge_rate = min([pilot, self.max_rate, self._capacity - self._current_charge])
-        if self._noise_level > 0:
-            charge_rate -= abs(np.random.normal(0, self._noise_level))
-        self._current_charge += charge_rate
-        self._current_charging_rate = charge_rate
-        return charge_rate
-
-
-class Linear2StageBattery(NoisyBattery):
+class Linear2StageBattery(Battery):
     """ Extends NoisyBattery with a simple piecewise linear model of battery dynamics based on SoC.
 
     Battery model based on a piecewise linear approximation of battery behavior. The battery will charge at the
@@ -124,14 +95,47 @@ class Linear2StageBattery(NoisyBattery):
 
         """
         if self._soc < self._transition_soc:
-            charge_rate = min(pilot, self.max_rate)
+            charge_rate = min([pilot, self.max_rate, self._capacity - self._current_charge])
             if self._noise_level > 0:
                 charge_rate -= abs(np.random.normal(0, self._noise_level))
         else:
-            charge_rate = min(pilot, ((1 - self._soc) / (1 - self._transition_soc) * self.max_rate))
+            charge_rate = min([pilot,
+                               ((1 - self._soc) / (1 - self._transition_soc) * self.max_rate),
+                               self._capacity - self._current_charge])
             if self._noise_level > 0:
                 charge_rate += np.random.normal(0, self._noise_level)
             charge_rate = min([charge_rate, pilot, self.max_rate])
+        self._current_charge += charge_rate
+        self._current_charging_rate = charge_rate
+        return charge_rate
+
+
+class NoisyBattery(Linear2StageBattery):
+    """ Extends Battery to model noise in the charging process.
+
+    Noise here is modeled as rectified Gaussian noise which is subtracted from the normal maximum charging
+    rate. This noise roughly models the behavior of the battery management system which might restrict battery
+    charging rate during the charging process.
+
+    All public attributes are the same as Battery.
+    """
+
+    def __init__(self, capacity, init_charge, max_rate, noise_level=0):
+        super().__init__(capacity, init_charge, max_rate, noise_level, transition_soc=1)
+
+    def charge(self, pilot):
+        """ Method to "charge" the battery include subtractive rectified Gaussian noise.
+
+        Args:
+            pilot (float): Pilot signal passed to the battery.
+
+        Returns:
+            float: actual charging rate of the battery.
+
+        """
+        charge_rate = min([pilot, self.max_rate, self._capacity - self._current_charge])
+        if self._noise_level > 0:
+            charge_rate -= abs(np.random.normal(0, self._noise_level))
         self._current_charge += charge_rate
         self._current_charging_rate = charge_rate
         return charge_rate
--- acnportal/acnsim/models/ev.py
+++ acnportal/acnsim/models/ev.py
@@ -4,13 +4,13 @@ from builtins import property
 class EV:
     """Class to model the behavior of an Electrical Vehicle (ev).
 
-    Attributes:
+    Args:
         arrival (int): Arrival time of the ev. [periods]
         departure (int): Departure time of the ev. [periods]
         requested_energy (float): Energy requested by the ev on arrival. [acnsim units]
-        max_rate (float): Maximum charging rate of the ev. [acnsim units]
-        session_id (str): Identifier of the session belonging to this ev.
         station_id (str): Identifier of the station used by this ev.
+        session_id (str): Identifier of the session belonging to this ev.
+        battery (Battery-like): Battery object to be used by the EV.
         energy_delivered (float): Amount of energy delivered to the ev so far. [acnsim units]
         current_charging_rate (float): Charging rate at the current time step. [acnsim units]
         remaining_demand (float): Energy which still needs to be delivered to meet requested_energy. [acnsim units]
@@ -18,13 +18,17 @@ class EV:
         percent_remaining (float): remaining_demand as a percent of requested_energy.
     """
 
-    def __init__(self, arrive, depart, requested_energy, station_id, session_id, battery):
+    def __init__(self, arrival, departure, requested_energy, station_id, session_id, battery, estimated_departure=None):
         # User Defined Parameters
-        self._arrival = arrive
-        self._departure = depart
-        self._requested_energy = requested_energy
+        self._arrival = arrival
+        self._departure = departure
         self._session_id = session_id
         self._station_id = station_id
+
+        # Estimate of session parameters
+        self._requested_energy = requested_energy
+        self._estimated_departure = estimated_departure if estimated_departure is not None else departure
+
 
         # Internal State
         self._battery = battery
@@ -32,54 +36,82 @@ class EV:
 
     @property
     def arrival(self):
+        """ Return the arrival time of the EV."""
         return self._arrival
 
     @arrival.setter
     def arrival(self, value):
+        """ Set the arrival time of the EV. (int) """
         self._arrival = value
 
     @property
     def departure(self):
+        """ Return the departure time of the EV. (int) """
         return self._departure
 
     @departure.setter
     def departure(self, value):
+        """ Set the departure time of the EV. (int) """
         self._departure = value
 
     @property
+    def estimated_departure(self):
+        """ Return the estimated departure time of the EV."""
+        return self._estimated_departure
+
+    @estimated_departure.setter
+    def estimated_departure(self, value):
+        """ Set the estimated departure time of the EV. (int) """
+        self._estimated_departure = value
+
+    @property
     def requested_energy(self):
+        """ Return the energy request of the EV for this session. (float) [acnsim units]. """
         return self._requested_energy
 
     @property
     def max_rate(self):
+        """ Return the maximum charging rate of the battery used by this EV. (float) """
         return self._battery.max_rate
 
     @property
     def session_id(self):
+        """ Return the unique session identifier for this charging session. (str) """
         return self._session_id
 
     @property
     def station_id(self):
+        """ Return the unique identifier for the EVSE used for this charging session. """
         return self._station_id
 
     @property
     def energy_delivered(self):
+        """ Return the total energy delivered so far in this charging session. (float) """
         return self._energy_delivered
 
     @property
     def current_charging_rate(self):
+        """ Return the current charging rate of the EV. (float) """
         return self._battery.current_charging_rate
 
     @property
     def remaining_demand(self):
+        """ Return the remaining energy demand of this session. (float)
+
+        Defined as the difference between the requested energy of the session and the energy delivered so far.
+        """
         return self.requested_energy - self.energy_delivered
 
     @property
     def fully_charged(self):
+        """ Return True if the EV's demand has been fully met. (bool)"""
         return not (self.remaining_demand > 1e-3)
 
     @property
     def percent_remaining(self):
+        """ Return the percent of demand which still needs to be furfilled. (float)
+
+        Defined as the ratio of remaining demand and requested energy. """
         return self.remaining_demand / self.requested_energy
 
     def charge(self, pilot):
