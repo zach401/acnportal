bf8c08604d5fd3e4d9c8a7ef4b6b2427c1bdde0b
Zachary Lee <zach401@gmail.com>
2019-06-17 09:47:27 -0700

    Update __init__.py
    Removed references to greedy algorithms.

--- acnportal/algorithms/__init__.py
+++ acnportal/algorithms/__init__.py
@@ -3,4 +3,3 @@ from .uncontrolled_charging import UncontrolledCharging
 from .sorted_algorithms import SortedSchedulingAlgo
 from .sorted_algorithms import RoundRobin
 from .sorted_algorithms import first_come_first_served, earliest_deadline_first, least_laxity_first
-from .greedy_algorithms import GreedyCostMinimization
