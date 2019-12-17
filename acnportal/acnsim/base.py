b0a350b0e87b3b8474d373ae836dcd1d01d26511
sunash <sunash.sharma@gmail.com>
2019-12-16 21:08:06 -0800

    Add serialization warnings and move JSON tests.

--- acnportal/acnsim/base.py
+++ acnportal/acnsim/base.py
@@ -2,28 +2,30 @@ import json
 import sys
 from functools import wraps
 from pydoc import locate
+import warnings
+
 
 def read_from_id(obj_id, context_dict, loaded_dict={}):
     if obj_id in loaded_dict:
         return loaded_dict[obj_id]
     if obj_id not in context_dict:
-        raise KeyError(f"Object with ID {obj_id} "
-                        "not found in context_dict.")
+        raise KeyError(f"Object with ID {obj_id} not found in context_dict.")
     obj_type = context_dict[obj_id]['class']
     obj_class = locate(obj_type)
 
     obj = obj_class.from_registry(
-        {'id': obj_id, 'context_dict': context_dict}, 
-        loaded_dict=loaded_dict
-    )
+        {'id': obj_id, 'context_dict': context_dict}, loaded_dict=loaded_dict)
 
     loaded_dict[obj_id] = obj
     return obj
+
 
 """
 Base class for all ACN-Sim objects. Includes functions for
 representation and serialization of ACN-Sim objects.
 """
+
+
 class BaseSimObj:
     def __repr__(self):
         """ General string representation of an ACN-Sim object. """
@@ -31,10 +33,7 @@ class BaseSimObj:
         for key, value in self.__dict__.items():
             attr_repr_lst.append(f'{key}={value}')
         attr_repr = ', '.join(attr_repr_lst)
-        return (
-            f'{self.__module__}.{self.__class__.__name__}'
-            f'({attr_repr})'
-        )
+        return f'{self.__module__}.{self.__class__.__name__}({attr_repr})'
 
     @classmethod
     def from_json(cls, in_json):
@@ -46,8 +45,8 @@ class BaseSimObj:
         try:
             obj_dict = context_dict[obj_id]
         except KeyError:
-            raise KeyError(f"Object with ID {obj_id} "
-                            "not found in context_dict.")
+            raise KeyError(
+                f"Object with ID {obj_id} not found in context_dict.")
 
         if obj_id is not None and obj_id in loaded_dict:
             return loaded_dict[obj_id]
@@ -57,20 +56,51 @@ class BaseSimObj:
                 f'{cls.__module__}.{cls.__name__}'
         except AssertionError:
             # TODO: Better message here.
-            warnings.warn("Deserializing subtype.")
+            warnings.warn("Deserializing subtype.", UserWarning)
 
         in_dict = obj_dict['args']
 
-        out_obj = cls.from_dict(in_dict, context_dict, 
-                                loaded_dict, cls_kwargs)
+        try:
+            out_obj = cls.from_dict(
+                in_dict, context_dict, loaded_dict, cls_kwargs)
+        except TypeError as err:
+            raise TypeError(
+                f"Encountered TypeError: {err} while loading object  of type "
+                f"{obj_dict['class']} using constructor  arguments for "
+                f"{cls.__module__}.{cls.__name__}.  The true constructor of "
+                f"{obj_dict['class']} may take more arguments. If this is "
+                 "the case, consider writing a from_dict method for "
+                f"{obj_dict['class']}."
+            )
+
+        if out_obj.__dict__.keys() != in_dict.keys():
+            unloaded_attrs = set(in_dict.keys()) - set(out_obj.__dict__.keys())
+            warnings.warn(
+                f"Attributes {unloaded_attrs} present in object of type "
+                f"{obj_dict['class']} but not handled by object's to_dict "
+                 "method. Loaded object may have inaccurate attributes.",
+                UserWarning
+            )
+            for attr in unloaded_attrs:
+                try:
+                    setattr(
+                        out_obj, attr,
+                        read_from_id(in_dict[attr], context_dict, loaded_dict)
+                    )
+                except KeyError:
+                    warnings.warn(
+                        f"Loader for attribute {attr} not found. Setting "
+                        f"attribute {attr} directly.",
+                        UserWarning
+                    )
+                    setattr(out_obj, attr, in_dict[attr])
 
         if obj_id is not None:
             loaded_dict[obj_id] = out_obj
         return out_obj
 
     @classmethod
-    def from_dict(cls, in_dict, context_dict, 
-                  loaded_dict, cls_kwargs={}):
+    def from_dict(cls, in_dict, context_dict, loaded_dict, cls_kwargs={}):
         raise NotImplementedError
 
     def to_json(self):
@@ -86,9 +116,44 @@ class BaseSimObj:
 
         args_dict = self.to_dict(context_dict)
 
-        obj_dict = {'class' : obj_type, 'args' : args_dict}
+        # Check that all attributes have been serialized
+        # Warn if some attributes weren't serialized
+        if args_dict.keys() != self.__dict__.keys():
+            unserialized_keys = \
+                set(self.__dict__.keys()) - set(args_dict.keys())
+            warnings.warn(
+                f"Attributes {unserialized_keys} present in object of type "
+                f"{obj_type} but not handled by object's to_dict method. "
+                 "Serialized object will not be loadable if any of these "
+                f"attributes appears in the constructor for {obj_type}.",
+                UserWarning
+            )
+            for key in unserialized_keys:
+                unserialized_attr = self.__dict__[key]
+                # Try calling the attr's to_registry method if it has one.
+                try:
+                    args_dict[key] = unserialized_attr.to_registry(
+                        context_dict=context_dict)['id']
+                    continue
+                except AttributeError:
+                    pass
+                # Try dumping the object using the native JSON serializer.
+                try:
+                    json.dumps(unserialized_attr)
+                except TypeError:
+                    warnings.warn(
+                        f"Attribute {key} could not be serialized. Dumping "
+                         "the attribute's repr() representation. This "
+                         "attribute will not be fully loaded.",
+                        UserWarning
+                    )
+                    args_dict[key] = repr(unserialized_attr)
+                else:
+                    args_dict[key] = unserialized_attr
+
+        obj_dict = {'class': obj_type, 'args': args_dict}
         context_dict[obj_id] = obj_dict
-        
+
         return {'id': obj_id, 'context_dict': context_dict}
 
     def to_dict(self, context_dict={}):
---
+++ acnportal/acnsim/tests/test_json_io.py
@@ -0,0 +1,404 @@
+from unittest import TestCase
+from unittest.mock import create_autospec
+
+from acnportal import acnsim
+from acnportal.algorithms import BaseAlgorithm, UncontrolledCharging
+
+import json
+import sys
+import numpy as np
+from copy import deepcopy
+from datetime import datetime
+
+# TODO: Integration test that completely compares simulator obj
+# before and after loading.
+# TODO: Call signature of from_registry: Class.from_registry()
+# TODO: Equality of simulator objects.
+# TODO: Repr for simulator objects.
+# TODO: Equality tests of objects
+class TestJSONIO(TestCase):
+    @classmethod
+    def setUpClass(self):
+        # Make instance of each class in registry.
+        # Battery
+        self.battery1 = acnsim.Battery(100, 50, 20)
+        self.battery1._current_charging_power = 10
+
+        # Linear2StageBattery
+        self.battery2 = acnsim.Linear2StageBattery(
+            100, 50, 20)
+        self.battery2._current_charging_power = 10
+        self.battery2._noise_level = 0.1
+        self.battery2._transition_soc = 0.85
+
+        # EVs
+        staying_time = 10
+        ev1_arrival = 10
+        self.ev1 = acnsim.EV(
+            ev1_arrival, ev1_arrival+staying_time, 30, 'PS-001', 'EV-001', deepcopy(self.battery1), 
+            estimated_departure=25
+        )
+        self.ev1._energy_delivered = 0.05
+        self.ev1._current_charging_rate = 10
+
+        ev2_arrival = 40
+        self.ev2 = acnsim.EV(
+            ev2_arrival, ev2_arrival+staying_time, 30, 'PS-002', 'EV-002',
+            deepcopy(self.battery2), estimated_departure=25
+        )
+        self.ev2._energy_delivered = 0.05
+        self.ev2._current_charging_rate = 10
+
+        ev3_arrival = 50
+        self.ev3 = acnsim.EV(
+            ev3_arrival, ev3_arrival+staying_time, 30, 'PS-003', 'EV-003',
+            deepcopy(self.battery2), estimated_departure=25
+        )
+        self.ev3._energy_delivered = 0.05
+        self.ev3._current_charging_rate = 10
+
+        # EVSEs
+        self.evse0 = acnsim.EVSE('PS-000', max_rate=32, 
+            min_rate=0)
+
+        self.evse1 = acnsim.EVSE('PS-001', max_rate=32, 
+            min_rate=0)
+        self.evse1.plugin(self.ev1)
+        self.evse1.set_pilot(30, 220, 1)
+
+        self.evse2 = acnsim.DeadbandEVSE('PS-002', max_rate=32,
+            min_rate=0, deadband_end=4)
+        self.evse2.plugin(self.ev2)
+        self.evse2.set_pilot(30, 220, 1)
+
+        self.evse3 = acnsim.FiniteRatesEVSE('PS-003', 
+            allowable_rates=[0, 8, 16, 24, 32])
+        self.evse3.plugin(self.ev3)
+        self.evse3.set_pilot(24, 220, 1)
+
+        # Events
+        self.event = acnsim.Event(0)
+        self.plugin_event1 = acnsim.PluginEvent(10, self.ev1)
+        self.unplug_event = acnsim.UnplugEvent(20, 'PS-001', 
+            'EV-001')
+        self.recompute_event = acnsim.RecomputeEvent(30)
+        self.plugin_event2 = acnsim.PluginEvent(40, self.ev2)
+        self.plugin_event3 = acnsim.PluginEvent(50, self.ev3)
+
+
+        # EventQueue
+        self.event_queue = acnsim.EventQueue()
+        self.event_queue.add_events([self.event, self.plugin_event1, 
+            self.recompute_event, self.plugin_event2,
+            self.plugin_event3])
+        
+        # Network
+        self.network = acnsim.ChargingNetwork()
+        self.network.register_evse(self.evse1, 220, 30)
+        self.network.register_evse(self.evse2, 220, 150)
+        self.network.register_evse(self.evse3, 220, -90)
+        self.network.constraint_matrix = np.array([[1, 0], [0, 1]])
+        self.network.magnitude = np.array([30, 30])
+        self.network.constraint_index = ['C1', 'C2']
+
+        # Simulator
+        self.simulator = acnsim.Simulator(
+            self.network, UncontrolledCharging(), 
+            self.event_queue, datetime(2019, 1, 1),
+            verbose=False,
+            store_schedule_history=True
+        )
+
+        # Make a copy of the simulator to run
+        self.simulator_run = deepcopy(self.simulator)
+        # Do necessary unplugs.
+        for station_id, evse in self.simulator_run.network._EVSEs.items():
+            if evse.ev is not None:
+                evse.unplug()
+        # Run simulation
+        self.simulator_run.run()
+
+        # Make a copy of the simulator with signals
+        self.simulator_signal = deepcopy(self.simulator)
+        self.simulator_signal.signals = {'a': [0, 1, 2], 'b': [3, 4]}
+
+        self.simulator_hard_signal = deepcopy(self.simulator)
+        self.simulator_hard_signal.signals = {'a': BaseAlgorithm()}
+
+    def test_battery_json(self):
+        battery_json = self.battery1.to_json()
+        battery_loaded = acnsim.Battery.from_json(battery_json)
+        self.assertIsInstance(
+            battery_loaded, acnsim.Battery)
+        self.assertEqual(
+            battery_loaded.__dict__, self.battery1.__dict__)
+
+    def test_linear_2_stage_battery_json(self):
+        battery_json = self.battery2.to_json()
+        battery_loaded = acnsim.Linear2StageBattery.from_json(battery_json)
+        self.assertIsInstance(
+            battery_loaded, acnsim.Linear2StageBattery)
+        self.assertEqual(
+            battery_loaded.__dict__, self.battery2.__dict__)
+
+    def test_ev_json(self):
+        ev_fields = ['_arrival', '_departure', '_requested_energy',
+            '_estimated_departure', '_session_id', '_station_id',
+            '_energy_delivered', '_current_charging_rate']
+
+        def _load_dump_compare_helper(ev, bat_type):
+            assert isinstance(ev._battery, bat_type)
+            ev_json = ev.to_json()
+            ev_loaded = acnsim.EV.from_json(ev_json)
+            self.assertIsInstance(ev_loaded, acnsim.EV)
+            for field in ev_fields:
+                self.assertEqual(getattr(ev, field), 
+                    getattr(ev_loaded, field))
+            self.assertIsInstance(ev_loaded._battery, bat_type)
+            self.assertEqual(ev_loaded._battery.__dict__,
+                ev._battery.__dict__)
+
+        _load_dump_compare_helper(self.ev1, acnsim.Battery)
+        _load_dump_compare_helper(self.ev2, 
+            acnsim.Linear2StageBattery)
+        _load_dump_compare_helper(self.ev3, 
+            acnsim.Linear2StageBattery)
+
+    def test_evse_no_ev_json(self):
+        evse_json = self.evse0.to_json()
+        evse_loaded = acnsim.EVSE.from_json(evse_json)
+        self.assertIsInstance(evse_loaded, acnsim.EVSE)
+
+        evse_fields = ['_station_id', '_max_rate', 
+            '_min_rate', '_current_pilot', 'is_continuous']
+
+        for field in evse_fields:
+            self.assertEqual(getattr(self.evse0, field), 
+                getattr(evse_loaded, field))
+        self.assertEqual(getattr(evse_loaded, '_ev'), None)
+
+    def test_evse_json(self):
+        evse_json = self.evse1.to_json()
+        evse_loaded = acnsim.EVSE.from_json(evse_json)
+        self.assertIsInstance(evse_loaded, acnsim.EVSE)
+
+        evse_fields = ['_station_id', '_max_rate', 
+            '_min_rate', '_current_pilot', 'is_continuous']
+
+        for field in evse_fields:
+            self.assertEqual(getattr(self.evse1, field), 
+                getattr(evse_loaded, field))
+        self.assertEqual(
+            getattr(getattr(evse_loaded, '_ev'), '_session_id'), 
+            'EV-001'
+        )
+
+    def test_deadband_evse_json(self):
+        evse_json = self.evse2.to_json()
+        evse_loaded = acnsim.DeadbandEVSE.from_json(evse_json)
+        self.assertIsInstance(evse_loaded, 
+            acnsim.DeadbandEVSE)
+
+        evse_fields = ['_station_id', '_max_rate',  '_min_rate',
+            '_current_pilot', '_deadband_end', 'is_continuous']
+
+        for field in evse_fields:
+            self.assertEqual(getattr(self.evse2, field), 
+                getattr(evse_loaded, field))
+        self.assertEqual(
+            getattr(getattr(evse_loaded, '_ev'), '_session_id'), 
+            'EV-002'
+        )
+
+    def test_finite_rates_evse_json(self):
+        evse_json = self.evse3.to_json()
+        evse_loaded = acnsim.FiniteRatesEVSE.from_json(evse_json)
+        self.assertIsInstance(evse_loaded, 
+            acnsim.FiniteRatesEVSE)
+
+        evse_fields = ['_station_id', '_max_rate',  '_min_rate',
+            '_current_pilot', 'is_continuous','allowable_rates']
+
+        for field in evse_fields:
+            self.assertEqual(getattr(self.evse3, field), 
+                getattr(evse_loaded, field))
+        self.assertEqual(
+            getattr(getattr(evse_loaded, '_ev'), '_session_id'), 
+            'EV-003'
+        )
+
+    def test_event_json(self):
+        event_json = self.event.to_json()
+        event_loaded = acnsim.Event.from_json(event_json)
+        self.assertIsInstance(event_loaded, acnsim.Event)
+
+        event_fields = ['timestamp', 'type', 'precedence']
+
+        for field in event_fields:
+            self.assertEqual(getattr(self.event, field), 
+                getattr(event_loaded, field))
+
+    def test_plugin_event_json(self):
+        event_json = self.plugin_event1.to_json()
+        event_loaded = acnsim.PluginEvent.from_json(event_json)
+        self.assertIsInstance(event_loaded, acnsim.PluginEvent)
+
+        event_fields = ['timestamp', 'type', 'precedence']
+
+        for field in event_fields:
+            self.assertEqual(getattr(self.plugin_event1, field), 
+                getattr(event_loaded, field))
+        self.assertEqual(
+            getattr(getattr(event_loaded, 'ev'), '_session_id'), 
+            'EV-001'
+        )
+
+    def test_unplug_event_json(self):
+        event_json = self.unplug_event.to_json()
+        event_loaded = acnsim.UnplugEvent.from_json(event_json)
+        self.assertIsInstance(event_loaded, acnsim.UnplugEvent)
+
+        event_fields = ['timestamp', 'type', 'precedence',
+            'station_id', 'session_id']
+
+        for field in event_fields:
+            self.assertEqual(getattr(self.unplug_event, field), 
+                getattr(event_loaded, field))
+
+    def test_recompute_event_json(self):
+        event_json = self.recompute_event.to_json()
+        event_loaded = acnsim.RecomputeEvent.from_json(event_json)
+        self.assertIsInstance(event_loaded, acnsim.RecomputeEvent)
+
+        event_fields = ['timestamp', 'type', 'precedence']
+
+        for field in event_fields:
+            self.assertEqual(getattr(self.recompute_event, field), 
+                getattr(event_loaded, field))
+
+    def test_event_queue_json(self):
+        event_queue_json = self.event_queue.to_json()
+        event_queue_loaded = acnsim.EventQueue.from_json(event_queue_json)
+        self.assertIsInstance(event_queue_loaded, acnsim.EventQueue)
+
+        self.assertEqual(self.event_queue._timestep, 
+            event_queue_loaded._timestep)
+
+        for (ts, event), (tsl, event_loaded) in \
+            zip(self.event_queue._queue, event_queue_loaded._queue):
+            self.assertEqual(ts, tsl)
+            self.assertIsInstance(event_loaded, acnsim.Event)
+            if not isinstance(event, acnsim.PluginEvent):
+                self.assertEqual(type(event), type(event_loaded))
+                self.assertEqual(event.__dict__, 
+                    event_loaded.__dict__)
+            else:
+                for field in ['timestamp', 'type', 'precedence']:
+                    self.assertEqual(
+                        getattr(event, field), 
+                        getattr(event_loaded, field)
+                    )
+                self.assertEqual(
+                    getattr(getattr(event_loaded, 'ev'), 
+                        '_session_id'), 
+                    getattr(getattr(event, 'ev'), 
+                        '_session_id')
+                )
+
+    def test_charging_network_json(self):
+        network_json = self.network.to_json()
+        network_loaded = acnsim.ChargingNetwork.from_json(network_json)
+        self.assertIsInstance(network_loaded, acnsim.ChargingNetwork)
+
+        network_np_fields = ['constraint_matrix', 'magnitudes', 
+            '_voltages', '_phase_angles']
+        for field in network_np_fields:
+            np.testing.assert_equal(getattr(self.network, field), 
+                getattr(network_loaded, field))
+
+        self.assertEqual(self.network.constraint_index, 
+            network_loaded.constraint_index)
+
+        for (station_id, evse), (station_id_l, evse_l) in \
+            zip(self.network._EVSEs.items(), 
+                network_loaded._EVSEs.items()):
+            self.assertEqual(station_id, station_id_l)
+            self.assertEqual(evse.station_id, evse_l.station_id)
+
+    def _sim_compare_helper(self, sim):
+        simulator_json = sim.to_json()
+        simulator_loaded = acnsim.Simulator.from_json(simulator_json)
+        self.assertIsInstance(simulator_loaded, acnsim.Simulator)
+
+        simulator_fields = ['period', 'max_recompute', 'verbose',
+            'peak', '_iteration', '_resolve',
+            '_last_schedule_update', 'schedule_history']
+        for field in simulator_fields:
+            self.assertEqual(getattr(sim, field), 
+                getattr(simulator_loaded, field))
+
+        if sys.version_info[1] < 7:
+            self.assertEqual(sim.start.isoformat(), simulator_loaded.start)
+        else:
+            self.assertEqual(sim.start, simulator_loaded.start)
+
+        self.assertIsInstance(simulator_loaded.scheduler, UncontrolledCharging)
+
+        if simulator_loaded.signals is not None:
+            self.assertEqual(sim.signals, simulator_loaded.signals)
+
+        np.testing.assert_equal(sim.pilot_signals,
+            simulator_loaded.pilot_signals)
+        np.testing.assert_equal(sim.charging_rates,
+            simulator_loaded.charging_rates)
+
+        # TODO: better proxy for network equality
+        network_attrs = ['station_ids', 'active_station_ids', 
+            'voltages', 'phase_angles']
+        for attr in network_attrs:
+            self.assertEqual(getattr(sim.network, attr),
+                getattr(simulator_loaded.network, attr))
+
+        # TODO: better proxy for event queue equality
+        # This only checks timestep and type
+        for (ts, event), (tsl, event_loaded) in \
+            zip(sim.event_queue._queue, 
+                simulator_loaded.event_queue._queue):
+            self.assertEqual(ts, tsl)
+            self.assertEqual(event.type, event_loaded.type)
+            self.assertEqual(event.timestamp, event_loaded.timestamp)
+
+        for event, event_loaded in \
+            zip(sim.event_history, 
+                simulator_loaded.event_history):
+            self.assertEqual(event.type, event_loaded.type)
+            self.assertEqual(event.timestamp, event_loaded.timestamp)
+
+        for station_id, ev in sim.ev_history.items():
+            self.assertEqual(ev.session_id, 
+                simulator_loaded.ev_history[station_id].session_id)
+
+    def test_init_simulator_json(self):
+        self._sim_compare_helper(self.simulator)
+
+    def test_run_simulator_json(self):
+        self._sim_compare_helper(self.simulator_run)
+
+    def test_object_equalities(self):
+        # Tests that certain object equality invariants are preserved
+        # after loading.
+        simulator_json = self.simulator_run.to_json()
+        simulator_loaded = acnsim.Simulator.from_json(simulator_json)
+
+        plugins = filter(lambda x: isinstance(x, acnsim.PluginEvent),
+            simulator_loaded.event_history)
+        evs = list(simulator_loaded.ev_history.values())
+        for plugin, ev in zip(plugins, evs):
+            self.assertIs(plugin.ev, ev)
+
+    def test_sim_signal_json(self):
+        self._sim_compare_helper(self.simulator_signal)
+
+    def test_sim_signal_warning(self):
+        with self.assertWarns(UserWarning):
+            self._sim_compare_helper(self.simulator_hard_signal)
