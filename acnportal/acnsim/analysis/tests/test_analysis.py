589041c3e493ae4c604c6ea7a88c034049abc1ee
sunash <sunash.sharma@gmail.com>
2020-02-07 16:26:27 -0800

    Add a unit test for the datetime_array warning.

--- acnportal/acnsim/analysis/tests/test_analysis.py
+++ acnportal/acnsim/analysis/tests/test_analysis.py
@@ -1,37 +1,27 @@
 """ Unit tests for analysis functions. """
+from datetime import datetime
 from unittest import TestCase
 from unittest.mock import Mock, create_autospec
 
-from acnportal import acnsim
-from acnportal.acnsim import Simulator
-from acnportal.acnsim.tests.test_simulator import TestSimulator
-from acnportal.acnsim import acndata_events
-from acnportal.acnsim import sites
-from acnportal.algorithms import BaseAlgorithm
-from datetime import datetime
-
-import pytz
 import numpy as np
-import os
-import json
-from copy import deepcopy
+
+from acnportal import acnsim
+from acnportal.algorithms import BaseAlgorithm
 
 
 class TestAnalysis(TestCase):
-    def test_datetimes_array(self):
+    def setUp(self):
         start = datetime(2018, 12, 31)
         network = acnsim.ChargingNetwork()
         evse1 = acnsim.EVSE('PS-001', max_rate=32)
         network.register_evse(evse1, 240, 0)
         scheduler = create_autospec(BaseAlgorithm)
         scheduler.max_recompute = None
-        events = acnsim.EventQueue(events=[acnsim.Event(1)])
-        simulator = Simulator(
-            network, scheduler, events, start, period=240)
-        simulator._iteration = 10
-
-        datetime_array = acnsim.datetimes_array(simulator)
-        expected_datetime_array = [
+        self.events = acnsim.EventQueue(events=[acnsim.Event(1)])
+        self.simulator = acnsim.Simulator(
+            network, scheduler, self.events, start, period=240)
+        self.simulator._iteration = 10
+        self.expected_datetime_array = [
             np.datetime64('2018-12-31T00:00:00.000000'),
             np.datetime64('2018-12-31T04:00:00.000000'),
             np.datetime64('2018-12-31T08:00:00.000000'),
@@ -43,4 +33,16 @@ class TestAnalysis(TestCase):
             np.datetime64('2019-01-01T08:00:00.000000'),
             np.datetime64('2019-01-01T12:00:00.000000')
         ]
-        np.testing.assert_equal(datetime_array, expected_datetime_array)
+
+    def test_datetimes_array_warning(self):
+        with self.assertWarns(UserWarning):
+            datetime_array = acnsim.datetimes_array(self.simulator)
+            np.testing.assert_equal(datetime_array,
+                                    self.expected_datetime_array)
+
+    def test_datetimes_array(self):
+        self.events.empty = Mock(self.events.empty)
+        self.events.empty = lambda : True
+        datetime_array = acnsim.datetimes_array(self.simulator)
+        np.testing.assert_equal(datetime_array,
+                                self.expected_datetime_array)
