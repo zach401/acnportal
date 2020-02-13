a6c7daf2f1f1a63603cdfbf8b23a6caacebbfb36
sunash <sunash.sharma@gmail.com>
2020-02-12 17:14:41 -0800

    Added gym conditional build for travis.
    Uses an environment variable specifying if gym should be
    installed in a shell script that dictates install
    instructions. Travis will thus test acnportal both with and
    without gym installed, allowing for testing of the
    machinery that checks if gym is installed within acnportal.

---
+++ .travis_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# .travis_script.sh - a script to allow conditional installs of
+# acnportal dependencies.
+
+# Set USE_GYM environment variable to default of True if it is not set.
+: "${USE_GYM:=True}"
+
+if $USE_GYM; then
+  pip install .[gym]
+else
+  pip install .
+fi
