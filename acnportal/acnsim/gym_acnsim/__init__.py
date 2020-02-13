711eb0455cdeff7f70b942b31c1768dc9a3e5b4d
sunash <sunash.sharma@gmail.com>
2020-02-12 18:01:07 -0800

    Add check for gym install in gym_acnsim __init__.py files.

--- acnportal/acnsim/gym_acnsim/__init__.py
+++ acnportal/acnsim/gym_acnsim/__init__.py
@@ -1,12 +1,15 @@
-from gym.envs.registration import register
 from . import reward_functions
+from importlib.util import find_spec
+if find_spec("gym") is not None:
+    from gym.envs.registration import register
 
-register(
-    id='default-acnsim-v0',
-    entry_point='acnportal.acnsim.gym_acnsim.envs:DefaultSimEnv',
-)
+    register(
+        id='default-acnsim-v0',
+        entry_point='acnportal.acnsim.gym_acnsim.envs:DefaultSimEnv',
+    )
 
-register(
-    id='rebuilding-acnsim-v0',
-    entry_point='acnportal.acnsim.gym_acnsim.envs:RebuildingEnv',
-)
+    register(
+        id='rebuilding-acnsim-v0',
+        entry_point='acnportal.acnsim.gym_acnsim.envs:RebuildingEnv',
+    )
+del find_spec
--- acnportal/acnsim/gym_acnsim/envs/__init__.py
+++ acnportal/acnsim/gym_acnsim/envs/__init__.py
@@ -1,3 +1,6 @@
-from .sim_prototype_env import BaseSimEnv
-from .sim_prototype_env import DefaultSimEnv
-from .sim_prototype_env import RebuildingEnv
+from importlib.util import find_spec
+if find_spec("gym") is not None:
+    from .sim_prototype_env import BaseSimEnv
+    from .sim_prototype_env import DefaultSimEnv
+    from .sim_prototype_env import RebuildingEnv
+del find_spec
