a16730139faa94e46cc9e065115b7fa43fe38936
Zixin Ye <GaryYe@Zixins-MacBook-Pro.local>
2020-04-30 23:31:09 -0700

    battery bound

--- acnportal/acnsim/models/battery.py
+++ acnportal/acnsim/models/battery.py
@@ -156,13 +156,13 @@ class Linear2StageBattery(Battery):
         if self._soc < self._transition_soc:
             charge_power = min([pilot * voltage / 1000, self._max_power, rate_to_full])
             if self._noise_level > 0:
-                charge_power -= abs(np.random.normal(0, self._noise_level))
+                charge_power = max(charge_power - abs(np.random.normal(0, self._noise_level)), 0)
         else:
             charge_power = min([pilot * voltage / 1000,
                                 (1 - self._soc) / (1 - self._transition_soc) * self._max_power,
                                 rate_to_full])
             if self._noise_level > 0:
-                charge_power += np.random.normal(0, self._noise_level)
+                charge_power = min(max(charge_power + np.random.normal(0, self._noise_level), 0), pilot * voltage / 1000, self._max_power, rate_to_full)
                 # ensure that noise does not cause the battery to violate any hard limits.
                 charge_power = min([charge_power, pilot * voltage / 1000, self._max_power, rate_to_full])
         self._current_charge += charge_power * (period / 60)
