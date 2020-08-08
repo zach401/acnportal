22af2fa504be77d0e4ab25b9fd1195ed251abc35
zach401 <zach401@gmail.com>
2020-08-07 18:09:49 -0700

    Update NpEncoder based on Python documentation.

--- acnportal/acnsim/base.py
+++ acnportal/acnsim/base.py
@@ -73,15 +73,15 @@ class ErrorAllWrapper:
 
 
 class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
+    def default(self, o):
+        if isinstance(o, np.integer):
+            return int(o)
+        elif isinstance(o, np.floating):
+            return float(o)
+        elif isinstance(o, np.ndarray):
+            return o.tolist()
         else:
-            return super(NpEncoder, self).default(obj)
+            json.JSONEncoder.default(self, o)
 
 
 class BaseSimObj:
