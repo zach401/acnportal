fc221d3124d8239dd8cf5dcb72edc75716eb6071
sunash <sunash.sharma@gmail.com>
2020-10-06 12:51:22 -0600

    Update UncontrolledCharging to use SessionInfo instead of EV objects.

--- acnportal/algorithms/uncontrolled_charging.py
+++ acnportal/algorithms/uncontrolled_charging.py
@@ -1,4 +1,9 @@
+# coding=utf-8
+""" Simple algorithm for uncontrolled EV charging. """
+from typing import List
+
 from .base_algorithm import BaseAlgorithm
+from ..acnsim.interface import SessionInfo
 
 
 class UncontrolledCharging(BaseAlgorithm):
@@ -7,31 +12,35 @@ class UncontrolledCharging(BaseAlgorithm):
     Implements abstract class BaseAlgorithm.
 
     This algorithm ignores all infrastructure constraints.
-    All EVs will be charged as quickly as possible according to their maximum charging rate and their battery dynamics.
-
+    All EVs will be charged as quickly as possible according to their maximum charging
+    rate and their battery dynamics.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
-        self.max_recompute = 1  # Call algorithm each period since it only returns a rate for the next period.
+        # Call algorithm each period since it only returns a rate for the next period.
+        self.max_recompute = 1
 
-    def schedule(self, active_evs):
-        """ Schedule each EV to charge as quickly as possible ignoring all infrastructure constraints.
+    def schedule(self, active_sessions: List[SessionInfo]):
+        """ Schedule each EV to charge as quickly as possible ignoring all
+        infrastructure constraints.
 
         Implements abstract method schedule from BaseAlgorithm.
 
         See class documentation for description of the algorithm.
 
-        This algorithm SHOULD NOT be run on physical infrastructure which has safety constraints as it could
-        (and likely will) violate those constraints.
+        This algorithm SHOULD NOT be run on physical infrastructure which has safety
+        constraints as it could (and likely will) violate those constraints.
 
         Args:
-            active_evs (List[EV]): see BaseAlgorithm
+            active_sessions (List[SessionInfo]): see BaseAlgorithm
 
         Returns:
             Dict[str, List[float]]: see BaseAlgorithm
         """
         schedule = {}
-        for ev in active_evs:
-            schedule[ev.station_id] = [self.interface.max_pilot_signal(ev.station_id)]
+        for session in active_sessions:
+            schedule[session.station_id] = [
+                self.interface.max_pilot_signal(session.station_id)
+            ]
         return schedule
