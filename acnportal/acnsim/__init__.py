444dea77a9f81f80d5e6e6e9fa3d2a8463db66ae
sunash <sunash.sharma@gmail.com>
2020-10-15 17:20:44 -0600

    Add mypy to Travis config.

--- acnportal/acnsim/__init__.py
+++ acnportal/acnsim/__init__.py
@@ -1,7 +1,7 @@
 from .simulator import Simulator
 from .interface import Interface, InvalidScheduleError
-
+# TODO: star imports here are causing duplicate and conflicting imports.
 from .analysis import *
 from .events import *
 from .models import *
-from .network import *
+from .network import *  # type: ignore
