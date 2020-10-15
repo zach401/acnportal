b9c42d32b8b7e900faf3bfd2ee70c373a4f485ca
sunash <sunash.sharma@gmail.com>
2020-10-15 17:26:18 -0600

    Fix intentionally broken type hint.

--- acnportal/acnsim/__init__.py
+++ acnportal/acnsim/__init__.py
@@ -3,6 +3,6 @@ from .interface import Interface, InvalidScheduleError
 
 # TODO: star imports here are causing duplicate and conflicting imports.
 from .analysis import *
-from .events import *
+from .events import *  # type: ignore
 from .models import *
 from .network import *  # type: ignore
