6f73eba6034551109a9e1eb3ce077993e1cc62a9
Zachary Lee <zach401@gmail.com>
2021-02-18 09:03:50 -0800

    Update setup.py
    Increment version number in setup.py.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="acnportal",
-    version="0.2.2",
+    version="0.3.1",
     author="Zachary Lee, Sunash Sharma",
     author_email="ev-help@caltech.edu",
     url="https://github.com/zach401/acnportal",
